405acd736c0dc5dbe9e342007578d9f169145ee7
praneethgadagoni <pgadagoni@anergroup.com>
2025-01-21 16:03:26 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    setTodos([todo, ...todos]);
+  };
+
+  const removeTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>To-Do List</h1>
+      <TodoForm onSubmit={addTodo} />
+      <TodoList todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function TodoForm({ onSubmit }) {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+      isComplete: false,
+    });
+    setInput("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a to-do"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="todo-input"
+      />
+      <button className="todo-button">Add a Task </button>
+    </form>
+  );
+}
+
+export default TodoForm;
